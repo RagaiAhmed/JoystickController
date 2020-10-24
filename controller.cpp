0dc7a5b141882be0a6079a8c9f90840e400e320e
Ragai <ragaiawad@gmail.com>
2020-10-25 01:07:51 +0200

    LineEdit Validators
    To Validate text input are only numbers in the specified range

--- controller.cpp
+++ controller.cpp
@@ -1,11 +1,17 @@
 #include "controller.h"
 #include "ui_controller.h"
+#include <QIntValidator>
 
 Controller::Controller(QWidget *parent)
     : QMainWindow(parent)
     , ui(new Ui::Controller)
 {
     ui->setupUi(this);
+
+    // Validator for text input
+    QIntValidator *limiter = new QIntValidator(0,1000);
+    ui->HLineEdit->setValidator(limiter);
+    ui->VLineEdit->setValidator(limiter);
 }
 
 Controller::~Controller()
